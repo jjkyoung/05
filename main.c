9e7327ceef3a9c057c5cdb0c83ba7f1f2bd4d99c
ykk226 <ykk226@naver.com>
2022-10-04 17:24:56 +0900

    04

--- main.c
+++ main.c
@@ -4,18 +4,17 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char c;
-	int num = 0;
+	int sum = 0;
+	int x;
+	int i;
+	 
+	printf("정수를 입력하세요.");
+	scanf("%d", &x);
 	
-	printf("문자열을 입력하세요:");
-	while( (c=getchar()) != '\n' ) // 입력 문자가 개별문자가 나올때까지 반복  
-	{
-		if ( c>= '0' && c<= '9' )  // 입력된 글자(c)가 숫자인가? 
-		    num = num + 1;  // 그렇다면 num에 하나를 센다. 
-	}
-	
-
-	printf("숫자의 개수는 %i개 입니다.\n", num); // 출력 
-	
+	  // for문을 활용해서 1부더 x까지 sum에 더하는 코드  
+	  for (i=0;i<=x;i++)  // 반복 (초기식;조건식;증감식)  
+	       sum = sum + i; // 더하기  
+    
+	printf("더하기 결과는 %i 입니다. \n", sum); //결과 출력  
 	return 0;
 }
